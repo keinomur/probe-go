6c342166f659bd0f2a26aace39f89f3a21e93087
keinomur <keinomur@yahoo-corp.jp>
2018-11-09 13:47:14 +0900

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+probe-go
+
+# about
+- CloudFoundry に異常がないかどうかを確認するための App です。
+- 200 だけ返します。
+
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "200")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	var port = os.Getenv("PORT")
+	if port == "" {
+		// no env variable for port
+		port = "8080"
+	}
+	fmt.Printf("Working on port:" + port)
+	http.ListenAndServe(":"+port, nil)
+}
